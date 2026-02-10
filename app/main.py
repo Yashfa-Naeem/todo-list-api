2af1827df42c281bf4db2ca029df3b1d1d3e63c0
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-02-10 17:31:11 +0500

    Setup FastAPI application with auth routes

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from api.auth.route import router as auth_router
+from api.database.database import engine, Base
+
+Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Todo List API", version="1.0.0")
+
+app.include_router(auth_router)
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome to Todo List API"}
