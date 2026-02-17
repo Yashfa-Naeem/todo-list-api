cfe40e3d5bf1c34fc11592c18ab7038f5de40400
Yashfa Naeem <yashfanaeem22@gmail.com>
2026-02-17 15:49:23 +0500

    Register reports router in main application

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from api.auth.route import router as auth_router
 from api.tasks.routes import router as task_router
+from api.reports.routes import router as reports_router
 from api.database.database import engine, Base
 
 Base.metadata.create_all(bind=engine)
@@ -9,6 +10,7 @@ app = FastAPI(title="Todo List API", version="1.0.0")
 
 app.include_router(auth_router)
 app.include_router(task_router)
+app.include_router(reports_router)
 
 @app.get("/")
 def read_root():
